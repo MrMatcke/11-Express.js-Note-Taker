65727e7c49a88bec5f5b453c73ecb5f4b91264f6
Mike Matcke <mr.matcke@gmail.com>
2021-02-25 21:48:20 -0800

    m

---
+++ Develop/routes.js
@@ -0,0 +1,37 @@
+//use the file system module (require method)
+
+
+
+
+//variable with rs.readFile method
+
+
+
+// Routing set up. using the get method route. 
+        // Read the db.json and returns notes as JSON.
+
+
+
+//Post method route set up for notes
+
+// Receives a new note, then returns the new one using json.
+
+
+
+
+// can use id/Retrieves a note with specific id
+
+
+
+
+
+//delete feature
+
+
+
+
+
+//routes
+
+
+
---
+++ Develop/server.js
@@ -0,0 +1,12 @@
+//require dependenies
+
+//intiialize express app
+
+
+//set up data parsing
+
+
+//require routes
+
+
+//set up listener
