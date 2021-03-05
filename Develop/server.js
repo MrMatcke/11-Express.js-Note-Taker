cd318cae197ac8cb8b0092cffe006112b6dbed79
Mike Matcke <mr.matcke@gmail.com>
2021-03-04 22:20:06 -0800

    updating server

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 const path = require('path');
 
+res.sendfile('./public/index.html');
+
 //for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
